e7ede0e4016874c6894d956dbe8fdd2883b25df0
joe <joe@jlcavazos.com>
2025-05-10 22:12:54 -0500

    Init.

---
+++ src/main/java/com/okta/androidoauthbff/AndroidOauthBffApplication.java
@@ -0,0 +1,13 @@
+package com.okta.androidoauthbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidOauthBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidOauthBffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okta/androidoauthbff/AndroidOauthBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okta.androidoauthbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidOauthBffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
